b9357b03cd4beace9ba0719b9dbf139472932115
Jason Awbrey <jawbrey@gmail.com>
2015-03-26 17:24:33 -0500

    site refresh
    remove old files, add new files

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,103 +1,163 @@
-/*global define */
-define(['jquery', 'skrollr'], function ($, skrollr) {
-    'use strict';
+/**
+ * Sticky Header
+ * --------------------------------------------------
+ */
+
+;(function($, window, document, undefined) {
+
+  'use strict';
+
+  var $win = $(window);
+  var $hero = $('.module--hero');
+  var $header = $('header');
+  var $body = $('body');
+  var $root = $('html, body');
+
+  $win.on('scroll', function() {
+    if ($(window).scrollTop() > ($hero.height() - $header.height())) {
+      $body.addClass('has-fixed-header');
+    } else {
+      $body.removeClass('has-fixed-header');
+    }
+  });
+
+  $('nav li a').click(function() {
+    var href = $.attr(this, 'href'),
+    	$trigger = $('#nav-trigger');
+
+    if($trigger.prop('checked')) {
+    	$trigger.attr('checked', false);
+    }
     
-    var $window = $(window), 
-    	$fixedNav = $(".fixedNav"), 
-    	lastScrollTop = $window.scrollTop(), 
-		wasScrollingDown = true, 
-		bannerHeight = $('.banner.hero').height(), 
-		initialFixedNavTop = $fixedNav.offset().top;
-
-
-        skrollr.init({
-            forceHeight: false
-        });
-
-
-        $(".scroll").click(function(event){
-             event.preventDefault();
-             //calculate destination place
-             var dest=0;
-             if($(this.hash).offset().top > $(document).height()-$(window).height()){
-                  dest=$(document).height()-$(window).height() - 51;
-             }else{
-                  dest=$(this.hash).offset().top - 51;
-             }
-             //go to destination
-             $('html,body').animate({scrollTop:dest}, 1000,'swing');
-         });
-
-/*
-   	$('.pl[data-type="background"]').each(function(){
-		var $bgobj = $(this); // assigning the object
-
-		$(window).scroll(function() {
-			// Scroll the background at var speed
-			// the yPos is a negative value because we're scrolling it UP!								
-			var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
-			
-			// Put together our final background position
-			var coords = '50% '+ yPos + 'px';
-
-			// Move the background
-			$bgobj.css({ backgroundPosition: coords });
-		
-		}); // window scroll Ends
-
- 	});
-*/
-
-    $window.scroll(function(event) {
-
-        var windowHeight = $window.height(), 
-         	fixedNavHeight = $fixedNav.outerHeight(), 
-
-         	scrollTop = $window.scrollTop(), 
-         	scrollBottom = scrollTop + windowHeight, 
-
-         	fixedNavTop = $fixedNav.offset().top, 
-         	fixedNavBottom = fixedNavTop + fixedNavHeight, 
-
-         	heightDelta = Math.abs((windowHeight - fixedNavHeight)), 
-         	scrollDelta = lastScrollTop - scrollTop, 
-
-         	isScrollingDown = (scrollTop > lastScrollTop), 
-         	isWindowLarger = (windowHeight > fixedNavHeight), 
-
-         	dragBottomDown = (fixedNavBottom <= scrollBottom && isScrollingDown),
-        	dragTopUp = (fixedNavTop >= scrollTop && !isScrollingDown),
-        	reachedBottom = '';
-
-     	//console.log(bannerHeight + ' ' + scrollTop);
-     	//console.log('fixedNavTop: ' + fixedNavTop + ', ' + 'fixedNavBottom: ' + fixedNavBottom)
-     	//console.log((bannerHeight - windowHeight) + fixedNavHeight + ': ' + scrollTop);
-
-         if(scrollTop >= bannerHeight && dragBottomDown) {
-         	$fixedNav.addClass('top');
-         }
-
-    	if(scrollTop < bannerHeight && dragTopUp) {
-         	$fixedNav.removeClass('top');
-         }
-		if(((bannerHeight - windowHeight) + fixedNavHeight) < scrollTop && dragBottomDown) {
-         	$fixedNav.removeClass('bottom');
-         }
-
-		if(!$fixedNav.hasClass('bottom') && scrollTop < ((bannerHeight - windowHeight) + fixedNavHeight)) {
-         	$fixedNav.addClass('bottom');
-         }
-        	 
-/*
-        console.log("dragBottomDown: " + dragBottomDown);
-        console.log("dragTopUp: " + dragTopUp);
-        
-        console.log("-------------------");
-*/
-
-        lastScrollTop = scrollTop;
-        wasScrollingDown = isScrollingDown;
-
+    $('nav li a').removeClass('active');
+	$(this).addClass('active');
+    $root.animate({
+        scrollTop: $(href).offset().top
+    }, 500, function () {
+        window.location.hash = href;
     });
+    return false;
+    
+  });
 
-});
+})(jQuery, window, document);
+
+
+
+
+
+/**
+ * Map
+ * --------------------------------------------------
+ */
+
+;(function($, window, document, undefined) {
+  L.mapbox.accessToken = 'pk.eyJ1IjoiamF3YnJleSIsImEiOiJnWFZxZDFJIn0.J8gkh8-6O-toLs5zD6wB-w';
+  var map = L.mapbox.map('map', 'jawbrey.gafj3efk');
+  map.setView([30.286475, -97.731209], 15);
+
+  // Build a marker from a simple GeoJSON object:
+  var marker = L.mapbox.featureLayer({
+      type: 'Feature',
+      geometry: {
+          type: 'Point',
+          coordinates: [-97.731209, 30.286475]
+      },
+      properties: {
+        title: '<h3 style="text-align:center;">Nico Muhly: How Little You Are</h3>',
+        description: '<div style="width:100%;text-align:center;">Conspirare @ Bass Concert Hall<br />April 18, 2015, 8 PM<br />More info at <a href="http://conspirare.org/event/nico-muhlys-how-little-you-are/">conspirare.org</a>.</div>',
+        'marker-size': 'large',
+        'marker-color': '#548cba',
+      }
+  }).addTo(map);
+
+  // Disable drag and zoom handlers.
+  map.dragging.disable();
+  map.touchZoom.disable();
+  map.doubleClickZoom.disable();
+  map.scrollWheelZoom.disable();
+
+  // Iterate over the featureLayer we've called "marker"
+  // and open its popup instead of clicking to trigger it.
+  marker.eachLayer(function(m) {
+    m.openPopup();
+  });
+})(jQuery, window, document);
+
+
+
+
+
+/**
+ * Navigation
+ * --------------------------------------------------
+ */
+
+;(function($, window, document, undefined) {
+  'use strict';
+  var $win = $(window),
+    active,
+    el;
+
+  // Creates a "named" logging function.
+	function setActive() {
+	  return function(_, a) {
+	  	$('nav li a').removeClass('active');
+	    $('nav li a.' + a).addClass('active');
+	    if(a !== 'hero') {
+	    	//window.location.replace('/#'+ a);
+	    	//window.location.hash = a;
+	    	history.pushState(null, null, '#'+a);
+        if (window._gaq !== undefined) { window._gaq.push(['_trackPageview', '/'+a]); }
+	    } else {
+	    	history.pushState(null, null, '/');
+        if (window._gaq !== undefined) { window._gaq.push(['_trackPageview', '/']); }
+	    	//window.location.hash = '';
+	    }
+      console.log(a);
+      if(a === 'biography' || a === 'audio' || a === 'repertoire') {
+        $('label[for="nav-trigger"]').addClass('dark');
+      } else {
+        $('label[for="nav-trigger"]').removeClass('dark');
+      }
+	  };
+	}
+
+	$.subscribe('section.active', setActive('section.active'));
+
+	$win.on('scroll', function() {
+		
+		if(document.elementFromPoint(0, 49).id) {
+			el = document.elementFromPoint(0, 49).id;
+		}
+		if(active !== el) {
+			active = el;
+			$.publish('section.active', el);
+		} 
+	});
+
+
+})(jQuery, window, document);
+
+
+
+
+
+/**
+ * Footer
+ * --------------------------------------------------
+ */
+
+;(function($, window, document, undefined) {
+
+	'use strict';
+
+  	var currYear = new Date().getFullYear();
+  	$('footer .copyright').html('&copy;' + currYear + ' Jason Awbrey. All rights reserved.');
+
+})(jQuery, window, document);
+
+
+
+
+
