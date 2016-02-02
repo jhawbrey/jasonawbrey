ffd3e253ea1876d9662bf8c9fd2665b74a17f46a
Jason Awbrey <jawbrey@gmail.com>
2016-02-02 17:18:12 -0600

    adding code for Heroku deploy

---
+++ web.js
@@ -0,0 +1,8 @@
+var compression = require('compression');
+var express = require('express')
+var serveStatic = require('serve-static')
+var app = express()
+
+app.use(compression())
+app.use(serveStatic(__dirname + '/dist'))
+app.listen(process.env.PORT || 5000)
